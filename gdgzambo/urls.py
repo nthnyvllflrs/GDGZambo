b338658b77eaeeb5767b6360ba455b6b2c2acc55
NthnyVllflrs <TS201402078@gmail.com>
2019-02-22 02:01:42 +0800

    Staticfiles with Landing Page

--- gdgzambo/urls.py
+++ gdgzambo/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+from .views import (landing_page,)
 
+urlpatterns = [
+    path('', landing_page, name='landing-page'),
+    
     path('event/', include(('event.urls', 'event'), namespace='event')),
     path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
     path('story/', include(('story.urls', 'story'), namespace='story')),
     path('user/', include(('user.urls', 'user'), namespace='user')),
+
+    path('admin/', admin.site.urls),
 ]
